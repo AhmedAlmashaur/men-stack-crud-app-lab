3c14ac71c0aa0a7d3168f60d6cbe178f72789a99
aalma <aalma@example.com>
2024-12-24 20:04:14 +0300

    pages setup slightly | some routing explaination

--- server.js
+++ server.js
@@ -4,12 +4,17 @@ var app = express();
 
 
 /*------------------- Configuration ---------------------*/
+//method-override
+var methodOverride = require('method-override');
+app.use(methodOverride('_method'));
+
 // Morgan
 var morgan = require('morgan');
 app.use(morgan('dev'));
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
+
 
 // mongoose connection
 const dotenv = require('dotenv');
@@ -27,18 +32,55 @@ connect();
 /*------------------- Routes ---------------------*/
 // use res.render to load up an ejs view file
 
+// test route
+app.get('/test', function(req, res) {
+  res.render('pages/test');
+});
+
+// read module (index/read)
+app.get('/', function(req, res) {
+  res.render('pages/index');
+});
+
+// create module (/new)
+app.get('/new', function(req, res) {
+  res.render('pages/new');
+});
+
+// update module (index/update)
+app.get('/update/:id', function(req, res) {
+  res.render('pages/update');
+});
+
+// delete module (index/delete)
+app.get('/delete/:id', function(req, res) {
+  res.render('pages/delete');
+});
+
+/*------------------- GET routes ---------------------*/
+//Displays a list of all plants
+
+
+//Shows a form to create a new plant
+
+
+//Displays a specific plant by its ID
+
+
+//Shows a form to edit an existing plant
 
 
 
-/*------------------- CRUD elements ---------------------*/
+/*------------------- POST routes ---------------------*/
+//Creates a new plant
 
-// GET routes
 
-// POST routes
+/*------------------- PUT routes ---------------------*/
+//Updates a specific plant by its ID
 
-// PUT routes
 
-// DELETE routes
+/*------------------- DELETE routes ---------------------*/
+//Deletes a specific plant by its ID
 
 
 /*------------------- Server initialization ---------------------*/
